40e995871e11eceeb52023fa093ce11a8b4b6102
James Bowles <james.bowles@fluxfederation.com>
2017-11-16 15:39:56 +1300

    Display feedback on answers

--- public/app.js
+++ public/app.js
@@ -1,29 +1,74 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function (problemNumber) {
-    var view = $('.templates .problem-view').clone();
-    view.find('.title').text('Problem #' + problemNumber);
-    learnjs.applyObject(learnjs.problems[problemNumber -1], view);
-    return view;
+learnjs.problemView = function (data) {
+  var problemNumber = parseInt(data, 10);
+  var view = learnjs.template('problem-view');
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
+  return view;
+}
+
+learnjs.template = function (name) {
+  return $('.templates .' + name).clone();
+}
+
+learnjs.flashElement = function (elm, content) {
+  elm.fadeOut('fast', function () {
+    elm.html(content);
+    elm.fadeIn();
+  });
+}
+
+learnjs.buildCorrectFlash = function (problemNumber) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a')
+
+  if (problemNumber < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNumber + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're finished!");
+  }
+
+  return correctFlash;
 }
 
 learnjs.showView = function (hash) {
-    var routes = {
-        '#problem': learnjs.problemView
-    };
-    var hashParts = hash.split('-');
-    var viewFn = routes[hashParts[0]];
-    if (viewFn) {
-        $('.view-container').empty().append(viewFn(hashParts[1]));
-    }
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
 }
 
 learnjs.appOnReady = function () {
-    window.onhashchange = function() {
-        learnjs.showView(window.location.hash);
-    }
+  window.onhashchange = function () {
     learnjs.showView(window.location.hash);
+  }
+  learnjs.showView(window.location.hash);
 }
 
 learnjs.problems = [
@@ -37,8 +82,8 @@ learnjs.problems = [
   }
 ]
 
-learnjs.applyObject = function(obj, elm) {
-  for(var key in obj) {
+learnjs.applyObject = function (obj, elm) {
+  for (var key in obj) {
     elm.find('[data-name="' + key + '"]').text(obj[key]);
   }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -9,7 +9,7 @@ describe('LearnJS', function () {
     expect($('.view-container .landing-view').length).toEqual(1);
   });
 
-  it('invokes the router when loaded', function() {
+  it('invokes the router when loaded', function () {
     spyOn(learnjs, 'showView');
     learnjs.appOnReady();
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
@@ -29,9 +29,27 @@ describe('LearnJS', function () {
   });
 
   describe('problem view', function () {
+    var view;
+    beforeEach(function () {
+      view = learnjs.problemView('1');
+    })
+
     it('has a title that includes the problem number', function () {
-      var view = learnjs.problemView('1');
       expect(view.find('.title').text()).toEqual('Problem #1');
+    });
+
+    describe('answer section', function () {
+      it('checks the answer is correct', function () {
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+        expect(view.find('.correct-flash span').text()).toEqual('Correct! ');
+      });
+
+      it('rejects incorrect answers', function () {
+        view.find('.answer').val('false');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text()).toEqual('Incorrect!');
+      });
     });
   });
 });

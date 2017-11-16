ecf467ea69371024aa53bac038470cb92c502f44
James Bowles <james.bowles@fluxfederation.com>
2017-11-16 13:16:35 +1300

    Demo problems

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ var learnjs = {};
 learnjs.problemView = function (problemNumber) {
     var view = $('.templates .problem-view').clone();
     view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(learnjs.problems[problemNumber -1], view);
     return view;
 }
 
@@ -23,4 +24,21 @@ learnjs.appOnReady = function () {
         learnjs.showView(window.location.hash);
     }
     learnjs.showView(window.location.hash);
+}
+
+learnjs.problems = [
+  {
+    description: 'What is the truth?',
+    code: 'function problem() { return __; }'
+  },
+  {
+    description: 'Simple math',
+    code: 'function problem() { return 42 == 6 * __; }'
+  }
+]
+
+learnjs.applyObject = function(obj, elm) {
+  for(var key in obj) {
+    elm.find('[data-name="' + key + '"]').text(obj[key]);
+  }
 }
